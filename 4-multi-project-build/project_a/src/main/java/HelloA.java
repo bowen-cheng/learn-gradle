0d2eec2ca48768d0be42f4bac8e2aa9c172460a6
Bowen Cheng <blueicycky@gmail.com>
2018-01-24 08:03:00 -0500

    Demonstrate how to do multiproject build with Gradle

---
+++ 4-multi-project-build/project_a/src/main/java/HelloA.java
@@ -0,0 +1,5 @@
+public class HelloA {
+    public void run () {
+        System.out.println("Hello from Project A");
+    }
+}
---
+++ 4-multi-project-build/project_b/src/main/java/HelloB.java
@@ -0,0 +1,7 @@
+public class HelloB {
+    public static void main (String[] args) {
+        HelloA helloA = new HelloA();
+        System.out.println("Hello from Project B");
+        helloA.run();
+    }
+}
